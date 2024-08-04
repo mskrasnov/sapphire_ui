292f6c19ac2b6c7033a04e0964ac1601978cbf17
mskrasnov <linuxoid85@gmail.com>
2024-08-04 23:43:45 +0300

    [WIP] themes...

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 pub mod theme;
 
-#[cfg(feature="toml-themes")]
+#[cfg(feature = "toml-themes")]
 pub mod theme_config;
 
 pub mod accent;
 pub mod button;
+pub mod card;
+pub mod checkbox;
 pub mod header;
+pub mod radio;
+pub mod scrollable;
 pub mod text;
 pub mod text_input;
-pub mod radio;
-pub mod checkbox;
-pub mod scrollable;
 
 pub(crate) mod common;
 pub mod primary_container;
