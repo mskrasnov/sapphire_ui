ce3ae7d108b6ae84f73f4d678229736060d0a46f
mskrasnov <linuxoid85@gmail.com>
2024-08-11 05:38:08 +0300

    Update docs

--- src/widgets/button.rs
+++ src/widgets/button.rs
@@ -2,6 +2,12 @@ use crate::theme::button::NAME;
 use crate::theme::button::ButtonStyle;
 use crate::theme::prelude::*;
 
+use embed_doc_image::embed_doc_image;
+
+/// Creates `iced::widget::button::Button` widget
+///
+/// ![button][button]
+#[embed_doc_image("button", "assets/doc/button.png")]
 pub fn button<'a, B>(content: impl Into<Element<'a, B>>) -> widget::Button<'a, B> {
     widget::button(content)
         .padding([
