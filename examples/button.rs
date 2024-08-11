d801dbcf1239d008e1b866cd161faec3b050af35
mskrasnov <linuxoid85@gmail.com>
2024-08-11 05:38:20 +0300

    Use new widgets

--- examples/button.rs
+++ examples/button.rs
@@ -3,10 +3,13 @@ use sapphire_ui::theme::prelude::*;
 use sapphire_ui::widgets::button;
 use sapphire_ui::widgets::container;
 use sapphire_ui::widgets::scrollable;
+use sapphire_ui::widgets::small_text;
+use sapphire_ui::widgets::text;
+use sapphire_ui::widgets::text::title_text;
 
 use iced::widget::column;
 use iced::widget::row;
-use iced::widget::text;
+// use iced::widget::text;
 use iced::{Font, Sandbox, Settings};
 use widget::horizontal_space;
 use widget::vertical_space;
@@ -67,13 +70,16 @@ impl Sandbox for ButtonExample {
         let space = vertical_space().height(Length::Fixed(1000.));
         let space1 = vertical_space().height(Length::Fixed(1000.));
         let clmn = column![
+            title_text("Button example"),
+            text("Scroll me!"),
             row![space].spacing(10).width(Length::Fill),
             row![
                 horizontal_space().width(Length::Fill),
                 btn,
-                horizontal_space().width(Length::Fill)
+                horizontal_space().width(Length::Fill),
             ],
-            space1
+            space1,
+            small_text("Amazing!"),
         ]
         .spacing(10);
 
