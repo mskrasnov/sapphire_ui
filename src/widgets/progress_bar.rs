b583398e2e0de6b47297b2816d54016b7198e738
mskrasnov <linuxoid85@gmail.com>
2024-08-27 13:08:44 +0300

    Reformat

--- src/widgets/progress_bar.rs
+++ src/widgets/progress_bar.rs
@@ -4,5 +4,7 @@ use crate::theme::prelude::*;
 use crate::theme::progress_bar::ProgressBarStyle;
 
 pub fn progress_bar(range: RangeInclusive<f32>, value: f32) -> widget::ProgressBar {
-    widget::progress_bar(range, value).style(theme::ProgressBar::Custom(Box::new(ProgressBarStyle))).height(10)
+    widget::progress_bar(range, value)
+        .style(theme::ProgressBar::Custom(Box::new(ProgressBarStyle)))
+        .height(10)
 }
